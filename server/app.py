93c255c5fad2d0ee646f19d1de412b007f26eaf6
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2023-05-16 10:23:07 -0400

    final copy

--- server/app.py
+++ server/app.py
@@ -1,16 +1,124 @@
 #!/usr/bin/env python3
+from flask import Flask, request, session
+from flask_migrate import Migrate
+from models import User, Ticket, Events, db,datetime
+from flask_bcrypt import Bcrypt
+class TicketInfo:
+    def __init__(self, ticket, event):
+       self.ticket = ticket
+       self.event =event
 
-# Standard library imports
+    def to_dict(self):
+        return {
+            'ticket': self.ticket.to_dict(),
+            "event": self.event.to_dict()
+           
+        }    
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
-
-# Local imports
-from config import app, db, api
-from models import User, Recipe
-
-# Views go here!
+# from flask_restful import Resource
+app = Flask(__name__)
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
+db.init_app(app)
+bcrypt = Bcrypt(app)
+# AUTHORIZE FUNCTION
+def authorize():
+    user_id = session["user_id"]
+    current_user = User.query.get(user_id)
+    if not current_user:
+        return {'error': 'Not logged in'}, 401
+# USER ROUTES
+@app.post('/users')
+def register_user():
+    data = request.json
+    pw_hash = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(username=data['username'], password_hash=pw_hash, email=data['email'], age=int(data['age']), address=data['address'])
+    db.session.add(new_user)
+    db.session.commit()
+    session['user_id'] = new_user.id
+    return new_user.to_dict(), 200
+@app.get('/users')
+def get_users():
+    users = User.query.all()
+    return {'users': [user.to_dict() for user in users]}, 200
+@app.post('/login')
+def login():
+    data = request.json
+    user = User.query.where(User.username == data['username']).first()
+    if user and bcrypt.check_password_hash(user.password_hash, data['password']):
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+    else:
+        return {'error': 'Invalid credentials'}, 401
+@app.delete('/logout')
+def logout():
+    session.pop('user_id')
+    return {'message': 'Successfully logged out'}, 200
+# EVENTS ROUTES
+@app.get('/events')
+def get_events():
+    # authorize()
+    events = Events.query.all()
+    return {'events': [event.to_dict() for event in events]}, 200
+@app.get('/events/<int:id>')
+def get_event(id):
+    # authorize()
+    event = Events.query.get(id)
+    return event.to_dict(), 200
+@app.post('/events')
+def create_event():
+    # authorize()
+    print("here")
+    data = request.json
+    print(data)
+    date_for_events = '06/14/2024'
+    date_1 = datetime.strptime(date_for_events, "%m/%d/%Y"),
+    new_event = Events(name=data['name'], address=data['address'], description=data['description'], image=data['image'])
+    print(new_event)
+    db.session.add(new_event)
+    db.session.commit()
+    return new_event.to_dict(), 200
+    # return {"A":"B"}, 200
+@app.patch('/events/<int:id>')
+def update_event(id):
+    # authorize()
+    data = request.json
+    event = Events.query.where(Events.id == id).update(data)
+    db.session.commit()
+    event = Events.query.get(id)
+    return event.to_dict(), 200
+@app.delete('/events/<int:id>')
+def delete_event(id):
+    # authorize()
+    event = Events.query.get(id)
+    db.session.delete(event)
+    db.session.commit()
+    return {'message': 'Successfully deleted event'}, 200
+@app.post('/tickets')
+def buy_ticket():
+    data = request.json
+    new_ticket = Ticket(event_id = data['event_id'],user_id = session['user_id'])
+    db.session.add(new_ticket)
+    db.session.commit()
+    return new_ticket.to_dict(), 200
+@app.get('/tickets')
+def get_tickets():
+    # authorize()
+    tickets = Ticket.query.all()
+    event_info = []
+    for ticket in tickets: 
+        event = Events.query.get(ticket.event_id)
+        event_info.append({"ticket": ticket.to_dict(),"event":event.to_dict()})
+    return {'tickets':event_info}, 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+
+
+
+
--- server/models.py
+++ server/models.py
@@ -1,5 +1,121 @@
-from sqlalchemy_serializer import SerializerMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
+from sqlalchemy.orm import validates, relationships
+from datetime import datetime
+from sqlalchemy.ext.associationproxy import association_proxy
+db = SQLAlchemy()
 
-from config import db
 
-# Models go here!
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String)
+    email = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    tickets = db.relationship('Ticket', cascade="all, delete-orphan", backref='user')
+    event = association_proxy('tickets', 'event')
+    def __repr__(self):
+        return f'<User username={self.username} email={self.email} address={self.address}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'address': self.address
+        }
+    @validates('age')
+    def validate_age(self, key, age):
+        if age <18:
+            raise ValueError('Must be 18 or older to register')
+        return age
+    @validates('email')
+    def validate_email(self, key, email):
+        if '@' not in email:
+            raise ValueError('Invalid email address')
+        elif User.query.filter(User.email == email).first():
+            raise ValueError('Email already taken')
+        else:
+            return email
+    @validates('address')
+    def validate_address(self, key, address):
+        if len(address) < 10:
+            raise ValueError('Invalid address')
+        return address
+    @validates('username')
+    def validate_username(self, key, username):
+        duplicate_user = User.query.filter(User.username == username).first()
+        if duplicate_user:
+            raise ValueError('Username already taken')
+        else:
+            return username
+class Ticket(db.Model):
+    __tablename__ = 'tickets'
+    id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String, nullable=False)
+    # location = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
+    def __repr__(self):
+        return f'<Ticket {self.user_id} {self.event_id}'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            "event_id": self.event_id,
+           
+        }
+class Events(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique= True, nullable=False)
+    date = db.Column(db.DateTime, nullable=True)
+    time = db.Column(db.DateTime, nullable=True)
+    address = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    ticket = db.relationship('Ticket', cascade="all, delete-orphan", backref='event')
+    user = association_proxy('tickets', 'user')
+    def __repr__(self):
+        return f'<Events {self.name} {self.date} {self.time} {self.address} {self.description}>'
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'date': self.date,
+            'time': self.time,
+            'address': self.address,
+            'description': self.description,
+            'image': self.image
+        }
+    @validates('address')
+    def validate_address(self, key, address):
+        if len(address) < 5:
+            raise ValueError('Invalid address')
+        else:
+            return address
+    # @validates('date')
+    # def validate_date(self, key, date):
+    #     if date < datetime.now() or date == "":
+    #         raise ValueError('Invalid date')
+    #     else:
+    #         return date
+    @validates('description')
+    def validate_description(self, key, description):
+        if description == "":
+            raise ValueError('Description cannot be blank')
+        else:
+            return description
+
+
+
+
+
+
+
+
+
